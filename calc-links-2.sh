b9e4293654dc558eeaa5cd9dbd2f25daf3db5781
Pauloxc6 <94982491+Pauloxc6@users.noreply.github.com>
2024-06-01 19:22:44 -0300

    Update calc-links-2.sh

--- calc-links-2.sh
+++ calc-links-2.sh
@@ -29,5 +29,5 @@ if [[ $links_cont -ge 2 && $links_cont -le 10 ]]; then
     # Print
     echo -e "\e[37;1mA soma dos links é: \e[32;1m$soma Mbps\n\e[37;1mO menor link é: \e[32;1m$menor Mbps\n\e[37;1mQuantos Mbps em cada fila: \e[32;1m$mbps Mbps\e[0m"
 else
-    echo -e "\e[31;1mDeve ser inserido valores entre 2 e 5!\e[0m"
+    echo -e "\e[31;1mDeve ser inserido valores entre 2 e 10!\e[0m"
 fi
